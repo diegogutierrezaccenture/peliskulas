f5ef997608c053e13fc5aa39f9ccd6948fd9156b
diegogutierrezaccenture <d.gutierrez.gonzalez@accenture.com>
2024-01-18 10:47:19 +0100

    a

--- src/app/pages/auth/components/buttons-providers/button-providers.component.ts
+++ src/app/pages/auth/components/buttons-providers/button-providers.component.ts
@@ -29,7 +29,6 @@ export class ButtonProviders {
         try {
             const result = await this._authService.signInWithGoogleProvider();
             this._router.navigateByUrl('/');
-            console.log(result);
         } catch (error) {
             console.log(error);
         }
